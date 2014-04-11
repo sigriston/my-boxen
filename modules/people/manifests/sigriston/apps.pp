cfd4b9e19de5d089905bf95a5975fc71e89af21c
Thiago Sigrist <sigrist@gmail.com>
2014-04-11 16:25:13 -0300

    Fixing error, removing class entertainment.

--- modules/people/manifests/sigriston/apps.pp
+++ modules/people/manifests/sigriston/apps.pp
@@ -1,9 +1,8 @@
 class people::sigriston::apps {
   include people::sigriston::apps::browsers
+  include people::sigriston::apps::communication
   include people::sigriston::apps::development
   include people::sigriston::apps::editors
-  include people::sigriston::apps::entertainment
-  include people::sigriston::apps::communication
   include people::sigriston::apps::terminals
   include people::sigriston::apps::utilities
 }
