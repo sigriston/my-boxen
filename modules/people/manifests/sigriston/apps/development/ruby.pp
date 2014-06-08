cffb5ec7bc8487ba64f2296a7c892f87ff616726
Thiago Sigrist <sigrist@gmail.com>
2014-06-08 11:52:55 -0300

    WTF2, ruby changed to ruby_version I guess.

--- modules/people/manifests/sigriston/apps/development/ruby.pp
+++ modules/people/manifests/sigriston/apps/development/ruby.pp
@@ -1,7 +1,7 @@
 class people::sigriston::apps::development::ruby {
   # ruby packages
   ruby_gem { 'terminal-notifier for system':
-    gem  => 'terminal-notifier',
-    ruby => 'system'
+    gem          => 'terminal-notifier',
+    ruby_version => 'system'
   }
 }
