480b34f8876e81880b288414b374b186df7d2c36
Thiago Sigrist <sigrist@gmail.com>
2014-06-08 11:50:26 -0300

    WTF, ruby::gem changed to ruby_gem. :-(

--- modules/people/manifests/sigriston/apps/development/ruby.pp
+++ modules/people/manifests/sigriston/apps/development/ruby.pp
@@ -1,6 +1,6 @@
 class people::sigriston::apps::development::ruby {
   # ruby packages
-  ruby::gem { 'terminal-notifier for system':
+  ruby_gem { 'terminal-notifier for system':
     gem  => 'terminal-notifier',
     ruby => 'system'
   }
