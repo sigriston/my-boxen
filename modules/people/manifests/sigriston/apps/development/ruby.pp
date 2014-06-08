aa6458bc1a86a4526f9e14bc969f61868a6af845
Thiago Sigrist <sigrist@gmail.com>
2014-06-08 14:02:51 -0300

    Have terminal-notifier on all ruby versions.

--- modules/people/manifests/sigriston/apps/development/ruby.pp
+++ modules/people/manifests/sigriston/apps/development/ruby.pp
@@ -2,6 +2,6 @@ class people::sigriston::apps::development::ruby {
   # ruby packages
   ruby_gem { 'terminal-notifier for system':
     gem          => 'terminal-notifier',
-    ruby_version => 'system'
+    ruby_version => '*'
   }
 }
