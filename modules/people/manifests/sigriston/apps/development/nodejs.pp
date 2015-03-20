3f47948b87c7a51513f3aa25f495da5a9825178c
Thiago Sigrist <sigrist@gmail.com>
2015-03-20 10:47:00 -0300

    node 0.12

--- modules/people/manifests/sigriston/apps/development/nodejs.pp
+++ modules/people/manifests/sigriston/apps/development/nodejs.pp
@@ -1,74 +1,78 @@
 class people::sigriston::apps::development::nodejs {
 
-  nodejs::version { 'v0.10': }
+  nodejs::version { 'v0.12': }
 
-  # default to node v0.10
+  # default to node v0.12
   class { 'nodejs::global':
-    version => 'v0.10'
+    version => 'v0.12'
   }
 
   # NPM packages
   nodejs::module { 'bower':
-    node_version => 'v0.10'
+    node_version => 'v0.12'
   }
 
   nodejs::module { 'coffee-script':
-    node_version => 'v0.10'
+    node_version => 'v0.12'
   }
 
   nodejs::module { 'coffeelint':
-    node_version => 'v0.10'
-  }
-
-  nodejs::module { 'grunt-cli':
-    node_version => 'v0.10'
-  }
-
-  nodejs::module { 'jake':
-    node_version => 'v0.10'
-  }
-
-  nodejs::module { 'js-beautify':
-    node_version => 'v0.10'
-  }
-
-  nodejs::module { 'js-yaml':
-    node_version => 'v0.10'
-  }
-
-  nodejs::module { 'js2coffee':
-    node_version => 'v0.10'
+    node_version => 'v0.12'
   }
 
   nodejs::module { 'eslint':
-    node_version => 'v0.10'
+    node_version => 'v0.12'
   }
 
-  nodejs::module { 'jsonlint':
-    node_version => 'v0.10'
-  }
-
-  nodejs::module { 'less':
-    node_version => 'v0.10'
-  }
-
-  nodejs::module { 'LiveScript':
-    node_version => 'v0.10'
-  }
-
-  nodejs::module { 'npm-check-updates':
-    node_version => 'v0.10'
-  }
-
-  nodejs::module { 'sloc':
-    node_version => 'v0.10'
-  }
-
-  nodejs::module { 'yo':
-    node_version => 'v0.10'
+  nodejs::module { 'grunt-cli':
+    node_version => 'v0.12'
   }
 
   nodejs::module { 'html2jade':
-    node_version => 'v0.10'
+    node_version => 'v0.12'
+  }
+
+  nodejs::module { 'jake':
+    node_version => 'v0.12'
+  }
+
+  nodejs::module { 'js-beautify':
+    node_version => 'v0.12'
+  }
+
+  nodejs::module { 'js-yaml':
+    node_version => 'v0.12'
+  }
+
+  nodejs::module { 'js2coffee':
+    node_version => 'v0.12'
+  }
+
+  nodejs::module { 'jsonlint':
+    node_version => 'v0.12'
+  }
+
+  nodejs::module { 'karma-cli':
+    node_version => 'v0.12'
+  }
+
+  nodejs::module { 'less':
+    node_version => 'v0.12'
+  }
+
+  nodejs::module { 'LiveScript':
+    node_version => 'v0.12'
+  }
+
+  nodejs::module { 'npm-check-updates':
+    node_version => 'v0.12'
+  }
+
+  nodejs::module { 'sloc':
+    node_version => 'v0.12'
+  }
+
+  nodejs::module { 'yo':
+    node_version => 'v0.12'
   }
 }
